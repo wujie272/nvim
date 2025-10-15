b66b5e3d614f6a909fd7aa601d22cbcf5aab0505
jaye2720 <jaye2720@users.noreply.github.com>
2025-10-15 11:51:11 +0800

    backup 2025-10-15-1151

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,4 +1,4 @@
--- 要求必须首先这些模块
+-- 要求必须首先加载这些模块
 require("core.util")
 require("core.basic")
 require("core.keybind")
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -567,29 +567,26 @@ return
         },
     },
     {
-        "sphamba/smear-cursor.nvim",
-        event = "VeryLazy",
-        opts = {
-            stiffness = 0.5,
-            trailing_stiffness = 0.5,
-            matrix_pixel_threshold = 0.5,
-            -- Smear cursor when switching buffers or windows.
-            smear_between_buffers = true,
+      "sphamba/smear-cursor.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- 打开所有视觉增强
+        legacy_computing_symbols_support              = true,
+        legacy_computing_symbols_support_vertical_bars = true,
 
-            -- Smear cursor when moving within line or to neighbor lines.
-            -- Use `min_horizontal_distance_smear` and `min_vertical_distance_smear` for finer control
-            smear_between_neighbor_lines = true,
+        -- 让拖尾与背景融合更好
+        color_levels                   = 255,  -- 真彩
+        gamma                          = 2.2,
+        max_shade_no_matrix            = 0.5,
+        matrix_pixel_threshold         = 0.5,
+        volume_reduction_exponent      = 0.2,
+        minimum_volume_factor          = 0.6,
 
-            -- Draw the smear in buffer space instead of screen space when scrolling
-            scroll_buffer_space = true,
-
-            -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
-            -- Smears will blend better on all backgrounds.
-            legacy_computing_symbols_support = false,
-
-            -- Smear cursor in insert mode.
-           -- See also `vertical_bar_cursor_insert_mode` and `distance_stop_animating_vertical_bar`.
-            smear_insert_mode = true,
-        },
+        -- 垂直条光标专属
+        vertical_bar_cursor                     = true,
+        vertical_bar_cursor_insert_mode         = true,
+        distance_stop_animating_vertical_bar    = 0.1,
+        matrix_pixel_threshold_vertical_bar     = 0.2,
+      },
     },
 }
